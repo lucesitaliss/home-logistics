be4823c965a0490604fbed3037909921d1b80497
lucesitaliss <ing.liss87@gmail.com>
2024-09-21 14:07:28 -0300

    chance auth.ts

--- auth.ts
+++ auth.ts
@@ -22,14 +22,14 @@ export const {
     }),
   ],
   callbacks: {
-    // async jwt({ token, account }) {
-    //   if (account) {
-    //     token.accessToken = account.access_token;
-    //     token.refreshToken = account.refresh_token;
-    //     token.idToken = account.id_token;
-    //   }
-    //   return token;
-    // },
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.refreshToken = account.refresh_token;
+        token.idToken = account.id_token;
+      }
+      return token;
+    },
     async session({ session, token }) {
       session.accessToken = token.accessToken;
       token.refreshToken = token.refreshToken;
